5423e4f4e299075f25115b16c190a8bfbbd3661b
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-10-10 17:48:31 +0200

    rutas cambiadas

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
       <Routes>
         <Route path='/my-photo-dashboard/' exact element={<Home />} />
-        <Route path='/my-photo-dashboard/my-photos' element={<MyPhotos />} />
+        <Route path='/my-photo-dashboard/myphotos' element={<MyPhotos />} />
         <Route path='/my-photo-dashboard/search' element={<Search />} />
       </Routes>
 

c8d9b5e8bb9094e7153393a840675064e987f23d
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-10-13 11:15:28 +0200

    rutas cambiadas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { BrowserRouter } from "react-router-dom"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -11,11 +10,11 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

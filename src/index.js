79964370ede6a4fb9ee825f793dec772043a4b93
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-10-13 10:51:25 +0200

    rutas cambiadas

--- src/index.js
+++ src/index.js
@@ -11,11 +11,13 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

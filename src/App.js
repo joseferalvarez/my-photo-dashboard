33361c04fa6187d359f712375651755bc17753fc
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-10-14 10:26:20 +0200

    404 creado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Route, Routes, HashRouter } from 'react-router-dom';
 
 import Topbar from './components/topbar';
@@ -7,8 +6,8 @@ import Home from './pages/home.jsx';
 import MyPhotos from './pages/my-photos.jsx'
 import Search from './pages/search.jsx'
 import Footer from './components/footer';
+import NotFound from './pages/notFound';
 
-import './App.css';
 import "./styles/_blocks.scss";
 
 function App() {
@@ -19,7 +18,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/my-photos' element={<MyPhotos />} />
         <Route path='/search' element={<Search />} />
-        <Route path='*' element={<Home />} />
+        <Route path='/notfound' element={<NotFound />} />
       </Routes>
       <Footer />
     </HashRouter>
---
+++ src/pages/notFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src='../assets/images/notfound.png' alt="Error 404 not found" />
+            <h2>Hola</h2>
+        </div>
+    );
+}
+
+export default NotFound;
